1aad9f6eb2f94fbf4b006aa835a8850adb2f723f
trit7611-blip <trit7611@gmail.com>
2025-11-26 13:06:00 +0700

    Add files via upload

---
+++ BT c2/bài3c2.py
@@ -0,0 +1,14 @@
+width = 17
+height = 12.0
+a=width//2
+print("width//2 =",a)
+print(type(a))
+b=width/2.0
+print("width/2.0 =",b)
+print(type(b))
+c=height/3
+print("height/3 =",c)
+print(type(c))
+d=1 + 2 * 5
+print("1 + 2 * 5 =",d)
+print(type(d))    
